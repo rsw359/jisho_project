ce19a9a3ef859a4dec3eb2c95e8e6915fb48715a
Roger Wells <rsw359@gmail.com>
2024-08-28 12:29:22 +0900

    search view class added

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import KanjiElement, ReadingElement, Sense, Entry
 
-# Register your models here.
+admin.site.register(KanjiElement)
+admin.site.register(ReadingElement)
+admin.site.register(Sense)
+admin.site.register(Entry)
+
+
+
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Entry
+from django.db import models
 
-# Create your views here.
+class SearchView(TemplateView):
+  template_name = 'dictionary/search.html'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    query = self.request.GET.get('q')
+    if query:
+       #search across kanji, gloss, and readings fields
+       results = Entry.objects.filter(
+         models.Q(keb_elem__keb_icontains=query) |
+         models.Q(reb_elem__reb_icontains=query) |
+         models.Q(sense__gloss__icontains=query)
+         ).distinct()
+    else:
+      results = None
+
+    context['results'] = results
+    context['query'] = query
+
+    return context
